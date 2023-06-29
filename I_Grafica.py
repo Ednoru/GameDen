8c0b72e96a6c2d1961b73ffdf53f809bb4e0cd3f
Eduardo Chero <eduardochero08@gmail.com>
2023-06-28 21:30:46 -0500

    Cambios estéticos

--- I_Grafica.py
+++ I_Grafica.py
@@ -11,7 +11,7 @@ ventana.geometry("500x350")
 ventana.resizable(True, True)
 
 # fondo
-imagen = Image.open("fondo.jpeg")
+imagen = Image.open("./GameDen/fondo.jpeg")
 imagen = imagen.resize((ventana.winfo_screenwidth(), ventana.winfo_screenheight()))
 imagen_de_fondo = ImageTk.PhotoImage(imagen)
 fondo = tk.Label(ventana, image=imagen_de_fondo)
@@ -28,20 +28,20 @@ print(fuentes_disponibles)
 
 # Colocar el input en la interfaz
 label_juego = tk.Label(ventana, text="Ingrese el nombre del juego:")
-label_juego.grid(row=1, column=0)
+label_juego.grid(row=1, column=0, padx=10, pady=10, sticky=tk.W)
 
 entry_juego = tk.Entry(ventana)
-entry_juego.grid(row=1, column=1)
+entry_juego.grid(row=1, column=1, padx=10, pady=10, sticky=tk.W)
 
 label_nodos = tk.Label(ventana, text="Ingrese la cantidad de nodos:")
-label_nodos.grid(row=2, column=0)
+label_nodos.grid(row=2, column=0, padx=10, pady=10, sticky=tk.W)
 
 entry_nodos = tk.Entry(ventana)
-entry_nodos.grid(row=2, column=1)
+entry_nodos.grid(row=2, column=1, padx=10, pady=10, sticky=tk.W)
 
 # boton para generar el grafo
 boton = tk.Button(ventana, text="Generar grafo", command=lambda: ng.getNode(entry_juego.get(), int(entry_nodos.get())))
-boton.grid(row=3, column=0)
+boton.grid(row=3, column=0, padx=10, pady=10, sticky=tk.W)
 
 # Carga la ventana
 ventana.mainloop()
